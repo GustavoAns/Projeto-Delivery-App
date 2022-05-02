32a4ffba326ee17953771a18dc93d86aeaaf1006
Exodin1 <exodin1@outlook.com>
2022-05-02 14:55:33 -0300

    feat: creating salesProducts model

---
+++ back-end/src/database/models/salesProducts.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const SalesProducts = sequelize.define('SalesProducts', {
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   primaryKey: true,
+    // },
+    saleId: {
+      type: DataTypes.INTEGER,
+      field: 'sale_id',
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      field: 'product_id',
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      field: 'quantity',
+    },
+  }, {
+    timestamps: false,
+  });
+  return SalesProducts;
+}
