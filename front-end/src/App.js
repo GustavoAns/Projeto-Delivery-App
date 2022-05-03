5c83e88d25e27a18fe73f249157e014fdd0666cc
MartinSilva <martinempreendedor@gmail.com>
2022-05-03 18:37:33 -0300

    atualizar packag

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './assets/css/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import './assets/css/App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App" />
+    <Routes>
+      <Route
+        path="/"
+        element={ <Home /> }
+      />
+    </Routes>
+
   );
 }
 
