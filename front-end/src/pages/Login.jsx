a3b12d534d3e4b007f76807ca1943b4e1dc2de4d
Matheus Alves <malves224@gmail.com>
2022-05-06 20:46:35 -0300

    feat (Login): validação dos dados de login, caso invalido desabilita o botao

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,11 +3,34 @@ import { useNavigate, Link } from 'react-router-dom';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import storage from '../utils/localStorage';
 import api from '../utils/api';
+import validationEmail from '../utils/validations';
 
 export default function Login() {
   const [formValues, setFormValues] = useState({ email: '', password: '' });
+  const [alertStatus, setAlertStatus] = useState({ open: false, message: '' });
+  const dataTestId = {
+    inputEmail: 'common_login__input-email',
+    inputPassword: 'common_login__input-password',
+    buttonLogin: 'common_login__button-login',
+    buttonRegister: 'common_login__button-register',
+    alertInvalidEmail: 'common_login__element-invalid-email',
+  };
 
   const navigate = useNavigate();
+
+  const openAlert = (message) => {
+    const TIME_FOR_CLOSE = 5000;
+    setAlertStatus({ open: true, message });
+    setTimeout(() => setAlertStatus({ open: false, message: '' }), TIME_FOR_CLOSE);
+  };
+
+  const validateForm = () => {
+    const MIN_LENGTH_PASSWORD = 6;
+    const { email, password } = formValues;
+    const emailIsValid = validationEmail(email);
+    const passwordIsValid = password && +password.length >= MIN_LENGTH_PASSWORD;
+    return [emailIsValid, passwordIsValid].every((field) => field);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,14 +40,31 @@ export default function Login() {
       .post('/login', payload)
       .then(({ data }) => storage.set('token', data.token))
       .then(() => navigate('/customer/products'))
-      .catch((err) => console.error(err));
-  };
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    if (name === 'email') setFormValues({ ...formValues, email: value });
-    if (name === 'password') setFormValues({ ...formValues, password: value });
+      .catch(({ response: { data } }) => openAlert(data));
   };
 
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const alertElement = () => (
+    <p
+      value={ alertStatus.message }
+      data-testid={ dataTestId.alertInvalidEmail }
+    >
+      {alertStatus.message}
+    </p>
+  );
+
+  const {
+    inputEmail,
+    inputPassword,
+    buttonLogin,
+    buttonRegister } = dataTestId;
   return (
     <div className="bg-light d-flex justify-content-center align-items-center vh-100">
       <Form
@@ -37,6 +77,7 @@ export default function Login() {
             Login
           </Label>
           <Input
+            data-testid={ inputEmail }
             id="email"
             name="email"
             placeholder="email@trybeer.com.br"
@@ -44,12 +85,14 @@ export default function Login() {
             onChange={ handleInputChange }
             value={ formValues.email }
           />
+          { alertStatus.open && alertElement()}
         </FormGroup>
         <FormGroup>
           <Label for="password">
             Senha
           </Label>
           <Input
+            data-testid={ inputPassword }
             id="password"
             name="password"
             placeholder="***********"
@@ -59,16 +102,18 @@ export default function Login() {
           />
         </FormGroup>
         <Button
+          data-testid={ buttonLogin }
+          disabled={ !validateForm() }
           type="submit"
           color="success"
           className="px-4 mb-3"
           style={ { width: '17rem' } }
         >
           LOGIN
-
         </Button>
         <Link to="/register">
           <Button
+            data-testid={ buttonRegister }
             type="button"
             color="success"
             outline
