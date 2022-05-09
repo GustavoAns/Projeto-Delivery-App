083e283ee53c8efd76fefdc18694e92ce4a1cd97
Exodin1 <exodin1@outlook.com>
2022-05-09 20:29:22 -0300

    feat: adding checkout (not complete)

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 
 export default function Checkout() {
   return (
-    <div>Checkout</div>
+    <div>
+      <h2>Finalizar Pedido</h2>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Valor Unitário</th>
+            <th scope="col">Sub-Total</th>
+            <th scope="col">Remover Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* fazer um map dos produtos e preencher a tabela dinamicamente */}
+        </tbody>
+      </table>
+      <div>
+        `Total: R$
+        {}
+        `
+      </div>
+    </div>
   );
 }
