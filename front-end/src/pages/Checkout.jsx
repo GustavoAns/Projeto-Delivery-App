e2b4c561bc0e5b686411d558fba50130c9e3c5be
Exodin1 <exodin1@outlook.com>
2022-05-10 10:59:50 -0300

    feat: adding adress form

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -19,10 +19,39 @@ export default function Checkout() {
           {/* fazer um map dos produtos e preencher a tabela dinamicamente */}
         </tbody>
       </table>
-      <div>
-        `Total: R$
+      <div data-testid="customer_checkout__element-order-total-price">
+        Total: R$
         {}
-        `
+      </div>
+      <div>
+        <h2>Detalhes e Endereço para Entrega</h2>
+        <label htmlFor="vendor">
+          P. Vendedora Responsavel
+          <select
+            data-testid="customer_checkout__select-seller"
+            id="vendor"
+          >
+            <option>Fulana Pereira</option>
+          </select>
+        </label>
+        <label htmlFor="adress">
+          Endereço
+          <input
+            data-testid="customer_checkout__input-address"
+            type="text"
+            placeholder="Endereço"
+            id="adress"
+          />
+        </label>
+        <label htmlFor="number">
+          Número
+          <input
+            data-testid="customer_checkout__input-addressNumber"
+            type="text"
+            placeholder="Número"
+            id="number"
+          />
+        </label>
       </div>
     </div>
   );
